bb510b2e91577f4083ddfd4d5fa5ab6da328a007
Nathan Menge <nathan.menge@arcadiasolutions.com>
2018-10-23 13:49:36 -0400

    map accepts initalRegion from prop and updates its own region state on map moves

--- app/components/Map.js
+++ app/components/Map.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { MapView } from 'expo';
 
-const Marker = MapView.Marker
+const { Marker } = MapView
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { region: this.props.region }
+    this.regionUpdate = this.regionUpdate.bind(this)
+  }
+
+  regionUpdate(region) {
+    console.log("Region Updated!")
+    console.log(region)
+    this.setState({region: region})
+  }
+
   render() {
     const { region, markers } = this.props
     return (
       <MapView
         style={styles.container}
-        region={region}
+        initialRegion={region}
         showsUserLocation
         showsMyLocationButton
+        onRegionChangeComplete={e => this.regionUpdate(e)}
       >
       {markers}
       </MapView>
